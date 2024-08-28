67a245d2ff5a4b5bb972c90c86789107a4aef689
Jørgen Nordmoen <jorgen@nordmoen.net>
2024-08-28 14:03:08 +0200

    Initiell implementering av chat

---
+++ src/nks_slackbob/main.py
@@ -0,0 +1,151 @@
+"""Applikasjonsoppsett for Slack bot-en."""
+
+import functools
+import logging
+import re
+
+import httpx
+from slack_bolt import App
+from slack_sdk import WebClient
+
+from . import settings
+from .utils import USERNAME_PATTERN, convert_msg, is_bob_alive, strip_msg
+
+API_URL = httpx.URL(str(settings.kbs_endpoint))
+"""API URL til KBS systemet"""
+
+# Set opp logging med Slack
+logging.basicConfig(level=logging.INFO)
+
+# Sett opp Slack app for å koble til Slack
+app = App(token=settings.bot_token.get_secret_value())
+
+
+def chat(client: WebClient, event: dict[str, str]) -> None:
+    """Håndter et spørsmål på Slack ved å kalle NKS KBS."""
+    # Hent ut samtale historie før vi svarer ut noe
+    thread = event.get("thread_ts", event["ts"])
+    chat_hist = client.conversations_replies(channel=event["channel"], ts=thread)
+    # Start med å svare at vi jobber med et svar til bruker
+    temp_msg = client.chat_postMessage(
+        text="Kontakter kunnskapsbasen :robot_face:",
+        channel=event["channel"],
+        thread_ts=event["ts"],
+    )
+    # Lag en funksjon som kan brukes for å endre svar, dette gjør det enklere å
+    # ha komplekse funksjoner senere som bruker både 'text' og 'blocks' for å
+    # svare
+    update_msg = functools.partial(
+        client.chat_update,
+        channel=temp_msg.get("channel"),  # type: ignore[arg-type]
+        ts=temp_msg.get("ts"),  # type: ignore[arg-type]
+    )
+    # Sjekk tidlig om API-et kjører, slik at bruker slipper å vente
+    if not is_bob_alive(API_URL):
+        update_msg(text="Kunnskapsbasen kjører ikke akkurat nå :wrench:")
+        return
+    # Hent ut chat historikk og spørsmål fra brukeren
+    history = [convert_msg(msg) for msg in chat_hist.get("messages")[:-1]]  # type: ignore[index]
+    question = strip_msg(event["text"])
+    # Send spørsmål til NKS DS API
+    try:
+        reply = httpx.post(
+            API_URL.copy_with(path="/chat"),
+            json={"history": history, "question": question},
+            timeout=settings.answer_timeout,
+        )
+        if reply.status_code != 200:
+            app.logger.error(
+                "Mottok status %s og begrunnelse %s",
+                reply.status_code,
+                reply.reason_phrase,
+            )
+            update_msg(text="Ånei! Noe gikk galt for kunnskapsbasen :scream:")
+            return
+    except httpx.ReadTimeout:
+        app.logger.error(
+            "Spørring mot kunnskapbasen tok for lang tid, ventet %.1f sekunder!",
+            settings.answer_timeout,
+        )
+        update_msg(text="Kunnskapsbasen svarer ikke :shrug:")
+        return
+    data = reply.json()
+    context = {}
+    for cont in data["context"]:
+        context[cont["article_id"]] = cont
+    cites = "\n\n".join(
+        [
+            f"> {cite['text']}\n"
+            f"(_{context[cite['article_id']]['title'] or 'Uten tittel'}_ / "
+            f"_{cite['section'] or 'Uten seksjon'}_)"
+            for cite in data["quotes"]
+        ]
+    )
+    text = f"{data['answer']}\n{cites}"
+    # Hvis vi kommer ned hit så vet vi at systemet svarte på spørsmålet som
+    # forventet
+    update_msg(text=text)
+
+
+@app.event("app_mention")
+def slack_mention(event: dict[str, str], client: WebClient) -> None:
+    """Håndter @bot meldinger på Slack."""
+    app.logger.info(
+        "App mention fra bruker %s, i kanal %s, med tråd %s",
+        event["user"],
+        event["channel"],
+        event["ts"],
+    )
+    chat(client, event)
+
+
+@app.event("message")
+def thread_reply(event: dict[str, str], client: WebClient) -> None:
+    """Håndter svar i tråder boten har besvart."""
+    # Sjekk at meldingen er et svar i en tråd
+    if "thread_ts" not in event:
+        return
+    # Hvis det er svar i en tråd så sjekker vi om boten er involvert i tråden,
+    # hvis ikke så svarer vi ikke
+    history = client.conversations_replies(
+        channel=event["channel"], ts=event["thread_ts"]
+    )
+    we_replied = any(
+        [
+            msg["app_id"] == settings.id
+            for msg in history.get("messages")  # type: ignore[union-attr]
+            if "app_id" in msg
+        ]
+    )
+    if not we_replied:
+        return
+    # Det siste vi sjekker er om meldingen inneholder en '@bot' til oss, slike
+    # meldinger blir besvart av 'slack_mention' over og hvis vi ikke stopper
+    # prosessering her blir det to svar i tråden
+    for username in re.findall(USERNAME_PATTERN, event["text"]):
+        user = client.users_info(user=username)
+        if user.get("user")["profile"].get("api_app_id") == settings.id:  # type: ignore[index]
+            return
+    app.logger.info(
+        "Oppfølgningsspørsmål i tråd %s (kanal: %s), fra bruker %s",
+        event["ts"],
+        event["channel"],
+        event["user"],
+    )
+    chat(client, event)
+
+
+def main() -> None:
+    """Inngangsporten til Slack boten."""
+    from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+    try:
+        SocketModeHandler(app, app_token=settings.app_token.get_secret_value()).start()  # type: ignore[no-untyped-call]
+    except KeyboardInterrupt:
+        # Ignorer fordi det betyr at vi tester på kommandolinje og trenger ikke
+        # beskjed at noe feilet
+        pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/nks_slackbob/utils.py
@@ -0,0 +1,56 @@
+"""Hjelpefunksjoner som gjør livet enklere i møte med Slack."""
+
+import re
+from typing import Any
+
+import httpx
+
+USERNAME_PATTERN: re.Pattern[str] = re.compile(r"<@([A-Z0-9]+)>")
+"""Mønster for å kjenne igjen Slack brukernavn"""
+
+EMOJI_PATTERN: re.Pattern[str] = re.compile(r":([a-zA-Z0-9_-]+):")
+"""Mønster for å kjenne igjen Slack emoji"""
+
+QUOTE_PATTERN: re.Pattern[str] = re.compile(r"^>(.*)")
+"""Mønster for å kjenne igjen et Slack sitat"""
+
+QUOTE_LINK_PATTERN: re.Pattern[str] = re.compile(r"\(_(.*)_\)")
+"""Mønster for å kjenne igjen en Slack sitat forklaring/lenke"""
+
+
+def strip_msg(msg: str) -> str:
+    """Filtrer ut tekst i meldingen som vi ikke ønsker å sende til NKS KBS."""
+    # Filtrer ut Slack emoji
+    msg = re.sub(EMOJI_PATTERN, "", msg)
+    # Filtrer ut '@bruker' strenger
+    msg = re.sub(USERNAME_PATTERN, "", msg)
+    # Filtrer ut sitater
+    msg = re.sub(QUOTE_PATTERN, "", msg)
+    # Filtrer ut lenke til sitater
+    msg = re.sub(QUOTE_LINK_PATTERN, "", msg)
+    # Vi kjører 'strip' tilslutt slik at vi eventuelt fjerner mellomrom som
+    # oppstår fordi vi har fjernet tekst
+    return msg.strip()
+
+
+def convert_msg(slack_msg: dict[str, str]) -> dict[str, str]:
+    """Hjelpemetode som tar inn en Slack melding og konverterer til NKS KBS format."""
+    result: dict[str, Any] = {}
+    result["role"] = "ai" if "app_id" in slack_msg else "human"
+    text = strip_msg(slack_msg["text"])
+    if result["role"] == "ai":
+        result["content"] = dict(answer=text, quotes=[], context=[])
+    else:
+        result["content"] = text
+    return result
+
+
+def is_bob_alive(url: httpx.URL) -> bool:
+    """Sjekk om NKS KBS API er i live/oppe."""
+    api_url = url.copy_with(path="/is_alive")
+    try:
+        reply = httpx.get(api_url)
+        result: bool = reply.status_code == 200
+        return result
+    except httpx.ReadTimeout:
+        return False
