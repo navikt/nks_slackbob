424c84445311e54316cf0c19f782825f602d4fcb
Jørgen Nordmoen <jorgen@nordmoen.net>
2024-08-29 14:06:05 +0200

    Flere uttrykk for litt personlighet

--- src/nks_slackbob/expressions.py
+++ src/nks_slackbob/expressions.py
@@ -6,5 +6,7 @@ WORKING_ON_ANSWER = [
     "Skal sjekke om jeg finner ut av det :mage:",
     "La meg nå se...",
     "Skal se hva jeg finner ut av!",
+    "Det må jeg tenke litt på...",
+    "En ordentlig nøtt! La meg tenke litt...",
 ]
 """Svar boten kommer med for å indikere at den jobber med et svar"""
