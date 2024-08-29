04d695184692979fdb4d54e58cd9964902c6d3ac
Jørgen Nordmoen <jorgen@nordmoen.net>
2024-08-29 13:51:05 +0200

    Endret formuleringer

--- src/nks_slackbob/expressions.py
+++ src/nks_slackbob/expressions.py
@@ -1,10 +1,10 @@
 """Uttrykk som boten kan svare med."""
 
 WORKING_ON_ANSWER = [
-    "Oi, det var et godt spørsmål",
+    "Oi, det var et godt spørsmål!",
     "Godt spørsmål!",
-    "Skal sjekke om jeg finner ut av det",
+    "Skal sjekke om jeg finner ut av det :mage:",
     "La meg nå se...",
-    "Skal se hva jeg finner ut av",
+    "Skal se hva jeg finner ut av!",
 ]
 """Svar boten kommer med for å indikere at den jobber med et svar"""
