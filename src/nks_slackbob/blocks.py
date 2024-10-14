8b26f05cefb33a4f5e778db2dff1d5848c9fdd9c
Jørgen Nordmoen <jorgen@nordmoen.net>
2024-10-14 09:11:28 +0200

    Bruk tekstfragment for å utheve lenke

--- src/nks_slackbob/blocks.py
+++ src/nks_slackbob/blocks.py
@@ -2,6 +2,8 @@
 
 from typing import Any
 
+import httpx
+
 from .utils import markdown_to_slack
 
 
@@ -41,10 +43,41 @@ def context_block(msg: dict[str, Any]) -> dict[str, Any]:
 
 def cite_block(citation: dict[str, str], doc: dict[str, Any]) -> dict[str, Any]:
     """Formater en enkelt sitering som en Slack Block."""
+    cite_url = format_citation_url(
+        httpx.URL(doc["metadata"]["KnowledgeArticle_QuartoUrl"]), citation
+    )
     return {
         "type": "mrkdwn",
         "text": (
-            f"*<{doc['metadata']['KnowledgeArticle_QuartoUrl']}|{citation['title']}>*"
+            f"*<{cite_url}|{citation['title']}>*"
             f"\n_{markdown_to_slack(citation['text'])}_"
         ),
     }
+
+
+def format_citation_url(
+    base_url: httpx.URL, citation: dict[str, str], num_word: int = 4
+) -> httpx.URL:
+    """Formater sitering URL med text fragment.
+
+    Text Fragment gjør det mulig å be nettleseren om å utheve tekst på en side
+    man ikke har kontroll over (se
+    https://developer.mozilla.org/en-US/docs/Web/URI/Fragment/Text_fragments)
+
+    Args:
+        base_url:
+            Original URL til kunnskapsartikkelen
+        citation:
+            Siteringsstrukturen med tekst
+        num_word:
+            Antall ord å ta med i tekstfragmentet
+    Returns:
+        Ny URL med tekstfragment som uthever tekst
+    """
+    cite_text = citation.get("text", "")
+    if not cite_text:
+        return base_url
+    cite_words = cite_text.replace("\n", " ").split(" ")
+    start_fragment = " ".join(cite_words[0:num_word])
+    end_fragment = " ".join(cite_words[-1 - num_word : -1])
+    return base_url.copy_with(fragment=f":~:text={start_fragment},{end_fragment}")
